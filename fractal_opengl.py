414b3979b65d0b96f6d0347a2d180c39e77ac79e
xeratec <info@maketec.ch>
2016-10-21 11:48:02 +0200

    Changed layout and format code

--- fractal_opengl.py
+++ fractal_opengl.py
@@ -3,8 +3,9 @@
 @file fractal_opengl.py
 @author Philip Wiese
 @date 12 Okt 2016
-@brief Displays Mandelbrot Set with OpenGl
+@brief PyQt4 QGLWidget to displays Mandelbrot Set with OpenGl
 '''
+
 import sys
 # PyQt4 imports
 from PyQt4.QtCore import *
@@ -41,15 +42,17 @@ FS = """
 varying float xpos;
 varying float ypos;
 varying float zpos;
+uniform float step;
+
 void main (void)
 {
-    float iter = 0.0;
     float max_square = 3.0;
     float square = 0.0;
     float r = 0.0;
     float i = 0.0;
     float rt = 0.0;
     float it = 0.0;
+    float iter = 0.0;
     while(iter < 1.0 && square < max_square)
     {
         rt = (r*r) - (i*i) + xpos;
@@ -57,7 +60,7 @@ void main (void)
         r = rt;
         i = it;
         square = (r*r)+(i*i);
-        iter += 0.005;
+        iter += step;
     }
     gl_FragColor = vec4 (iter, iter, sin(iter*2.00), 1.0);
     //gl_FragColor = texture1D(tex, (i == iter ? 0.0 : float(i)) / 100.0);
@@ -66,8 +69,9 @@ void main (void)
 
 class Shader(object):
     shaderProgram = None
-    VAO = None
-    """Wrapper to create opengl 2.0 shader programms"""
+    #
+    # Wrapper to create OpenGL shader programms
+    #
     def __init__(self, vertex_source, fragment_source):
 
         self.vertexShader = self.compile_vertex_shader(vertex_source)
@@ -77,85 +81,103 @@ class Shader(object):
     def set_uniform_f(self, name, value):
         location = gl.glGetUniformLocation(self.shaderProgram, name)
         gl.glUniform1f(location, value)
-
+    
+    #
+    # Pass a variable to the shader
+    #
     def __setitem__(self, name, value):
-        """pass a variable to the shader"""
         if isinstance(value, float):
             self.set_uniform_f(name, value)
         else:
             raise TypeError("Only floats are supported so far")
-        
+    
+    #
+    # Compile a vertex shader from source 
+    #
     def compile_vertex_shader(self, source):
-        """Compile a vertex shader from source."""
         vertex_shader = gl.glCreateShader(gl.GL_VERTEX_SHADER)
         gl.glShaderSource(vertex_shader, source)
         gl.glCompileShader(vertex_shader)
-        # check compilation error
+        # Check compilation error
         result = gl.glGetShaderiv(vertex_shader, gl.GL_COMPILE_STATUS)
         if not(result):
             raise RuntimeError(gl.glGetShaderInfoLog(vertex_shader))
         return vertex_shader
     
+    #
+    # Compile a fragment shader from source
+    #
     def compile_fragment_shader(self, source):
-        """Compile a fragment shader from source."""
+        
         fragment_shader = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
         gl.glShaderSource(fragment_shader, source)
         gl.glCompileShader(fragment_shader)
-        # check compilation error
+        # Check compilation error
         result = gl.glGetShaderiv(fragment_shader, gl.GL_COMPILE_STATUS)
         if not(result):
             raise RuntimeError(gl.glGetShaderInfoLog(fragment_shader))
         return fragment_shader
     
+    #
+    # Create a shader program from compiled shaders
+    #
     def link_shader_program(self, vertex_shader, fragment_shader):
-        """Create a shader program with from compiled shaders."""
         program = gl.glCreateProgram()
         gl.glAttachShader(program, vertex_shader)
         gl.glAttachShader(program, fragment_shader)
         gl.glLinkProgram(program)
-        # check linking error
+        # Check linking error
         result = gl.glGetProgramiv(program, gl.GL_LINK_STATUS)
         if not(result):
             raise RuntimeError(gl.glGetProgramInfoLog(program))
         return program
 
+#
+# PyQt4 Widget to display Mandelbrot set with OpenGL
+#
 class GLWidget(QGLWidget):
-    # default window size
-    width, height = 600, 600
+    #
+    # Initialize GLWidget, init variables and set parent
+    #
     def __init__(self, parent=None):
         QGLWidget.__init__(self)
         self.real = -2.0
         self.w = 2.5
         self.imag = -1.25
         self.h = 2.5
+        self.step = 0.00
+        # Activate Mousetracking for mouseMoveEvent
         self.setMouseTracking(True)
         self.parent = parent
-        
+    
+    #
+    # Initialize OpenGL 
+    #    
     def initializeGL(self):
-        self.show_fps = False
-        
-        """Initialize OpenGL, VBOs, upload data on the GPU, etc."""
-        # background color
+        # Set background color
         gl.glClearColor(0.5,0.5,0.5,0.5)
-        # compile the shaders
+        # Compile the shader
         self.shader = Shader(vertex_source=VS, fragment_source=FS)
         self.shaders_program = self.shader.shaderProgram
-
+    
+    #
+    # Paint the scene
+    #
     def paintGL(self):
-        """Paint the scene."""
-        # clear the buffer
+        # Clear the buffer
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-        # bind the VBO
         
         gl.glLoadIdentity()
         gl.glUseProgram(self.shaders_program)
-        
+        # Set variables in Shader
         self.shader["real"] = self.real
         self.shader["w"] = self.w
         self.shader["imag"] = self.imag
         self.shader["h"] = self.h
-        
+        self.shader["step"] = self.step
+                
         gl.glBegin(gl.GL_QUADS)
+        # Draw a rect with vertexes and set texture coordinates
         gl.glTexCoord2f(0, 0)
         gl.glVertex2f(-1, -1)
         gl.glTexCoord2f(1, 0)
@@ -166,78 +188,92 @@ class GLWidget(QGLWidget):
         gl.glVertex2f(-1, 1)
         gl.glEnd()
         gl.glUseProgram(0)
-
+    
+    #
+    # Called upon window resizing: reinitialize the viewport.
+    #
     def resizeGL(self, width, height):
-        """Called upon window resizing: reinitialize the viewport."""
-        # update the window size
+        # Update the window size
         self.width, self.height = width, height
-        # paint within the whole window
+        # Paint within the whole window
         gl.glViewport(0, 0, width, height)
+        # Recalculate Mandelbrot dimensions
+        self.setCoord(self.real, self.imag, self.w)
         
     #
     # Calcualtes precision in digital digits
     #
-    def setCoord(self, re_min, im_min, w, h):
+    def setCoord(self, re_min, im_min,delta):
         # Calculate precision in decimal digits
         decimals = 3
-        for v in (w, h):
-            if abs(v) <= 1:
-                decimals = round(np.log10(1 / abs(v))) + 2
+        if abs(delta) <= 1:
+            decimals = round(np.log10(1 / abs(delta))) + 2
         
         # Round values with calculated precision
         self.real = round(re_min, int(decimals))
         self.imag = round(im_min, int(decimals))
-        self.w = round(w, int(decimals))
-        self.h = round(h, int(decimals))
+        self.w = round(delta, int(decimals+1))
+        self.h = round(self.height*delta/self.width, int(decimals))
         
         if self.parent is not None:
             # Update textbos values
             self.parent.textbox_re_min.setText(str(self.real))
-            self.parent.textbox_re_max.setText(str(self.real + self.w))
             self.parent.textbox_im_min.setText(str(self.imag))
-            self.parent.textbox_im_max.setText(str(self.imag + self.h))
+            self.parent.textbox_delta.setText(str(self.w))
+            
+    def setIter(self, max_iter):
+        self.step = 1.0/max_iter
     
+    #
+    # Zoom in or out by a given factor
+    #
     def zoom(self, factor):
+        # Zoom in
         if factor >0:
             re_min = self.real+factor/4.0 * self.w
             im_min = self.imag+factor/4.0 * self.h
             w = self.w/(2.0*factor)
-            h = self.h/(2.0*factor)
+        # Zoom out
         if factor <0:
             re_min = self.real-abs(factor)/2.0 * self.w
             im_min = self.imag-abs(factor)/2.0 * self.h
             w = self.w*2.0*abs(factor)
-            h = self.h*2.0*abs(factor)
-        
-        self.setCoord(re_min, im_min, w, h)
-                
+        self.setCoord(re_min, im_min, w)
+    
+    #
+    # Mouse press event
+    #            
     def mousePressEvent(self, event):
+        # Save mouse position to calculate relative movement
         pos = event.pos()
         self.buffer = (pos.x(), pos.y())
-        
+    
+    #
+    # Move viewport with left drag
+    #   
     def mouseMoveEvent(self, event):
         if event.buttons() == Qt.RightButton:
             return 0
         elif event.buttons() == Qt.LeftButton:
             pos = event.pos()
+            # Calculate relative movement
             d = [self.buffer[0]-pos.x(), self.buffer[1]-pos.y()]
             d[0] = self.w/self.width *d[0]
             d[1] = self.h/self.height * d[1]
             
-            self.setCoord(self.real+d[0], self.imag-d[1], self.w, self.h)
+            self.setCoord(self.real+d[0], self.imag-d[1], self.w)
             self.buffer = (pos.x(), pos.y())            
             self.repaint()
-            
+
+#
+# Main function for debugging
+#           
 if __name__ == '__main__':
-    data = np.zeros((10000, 2), dtype=np.float32)
-    data[:,0] = np.linspace(-1., 1., len(data))
-    
     print "[Debug] fractal_opengl"
     app = QApplication(sys.argv)
     form = QMainWindow()
     form.setWindowTitle('Mandelbrot Set')
     form.main_frame = GLWidget()
-    form.main_frame.data = data
     form.setCentralWidget(form.main_frame)  
     form.show()
     app.exec_()
--- fractal_qt4_opengl.py
+++ fractal_qt4_opengl.py
@@ -5,23 +5,20 @@
 @date 12 Okt 2016
 @brief Displays Mandelbrot Set with OpenGl
 '''
+
 import sys
-
 from fractal_opengl import GLWidget
-
+# PyQt4 Imports
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-from numpy import log10
 
-
-######  Config #######
+###  Start Config ####
 re_min = 0.385
-re_max = 0.395
 im_min = 0.135
-im_max = 0.145
+delta = 0.01
 
 max_betr = 2
-max_iter = 100      
+max_iter = 200      
 ######################
         
 class AppForm(QMainWindow):
@@ -34,20 +31,17 @@ class AppForm(QMainWindow):
         self.create_main_frame()
         self.create_status_bar()
 
-        #
         # Initialize textbox values
-        #
         self.textbox_re_min.setText(str(re_min))
-        self.textbox_re_max.setText(str(re_max))
         self.textbox_im_min.setText(str(im_min))
-        self.textbox_im_max.setText(str(im_max))
+        self.textbox_delta.setText(str(delta))
         self.textbox_max_iter.setText(str(max_iter))
         
-        #
         # Render mandelbrot set
-        #
         self.setMinimumWidth(620)
-        self.resize(620, 460)        
+        self.resize(620, 460)   
+        
+        self.on_draw()     
 
     #
     # Display infos about application
@@ -55,88 +49,82 @@ class AppForm(QMainWindow):
     def on_about(self):
         msg = """Mandelbrot Set Generator:
         
-    ### Features ###
-     * Click left mouse button and drag to zoom
-     * Enter custom values for ReMin, ReMin, ImMin and ImMax 
-     * Show or hide the grid
-     * Save the plot to a file using the File menu
-     * De-/activate continuous color spectrum
-     * De-/activate normalized values
+     ### Features ###
+     * Zoom in our out by clicking +/- Button
+     * Drag with the mouse to move the viewport
      
      ### Used Libraries ###
      * PyQt4
-     * Matplotlib
+     * PyOpenGL
+     * Numpy
      
      ### Author ###
      Made by Philip Wiese
      info@maketec.ch
-     16. Oktober 2016
+     20. Oktober 2016
         """
         QMessageBox.about(self, "About the demo", msg.strip())
+    
     #
     # Calculates mandelbrot set and updates mpl plot
     #    
     def on_draw(self):
         # Grap values from textboxes
         re_min = float(unicode(self.textbox_re_min.text()))
-        re_max = float(unicode(self.textbox_re_max.text()))
         im_min = float(unicode(self.textbox_im_min.text()))
-        im_max = float(unicode(self.textbox_im_max.text()))
+        delta = float(unicode(self.textbox_delta.text()))
         max_iter = int(unicode(self.textbox_max_iter.text()))
-         
-        self.glWidget.real = re_min
-        self.glWidget.w = re_max-re_min
-        self.glWidget.imag = im_min
-        self.glWidget.h = im_max-im_min
+        
+        # Pass values to GLWidget
+        self.glWidget.setCoord(re_min, im_min, delta)
+        self.glWidget.setIter(max_iter)
         self.glWidget.repaint()
         self.glWidget.setFocus()
         
         return 0 
     
+    #
+    # Key Press Event for zooming and drawing
+    #
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Enter: 
+        # Repaint with Enter and Return
+        if event.key() == Qt.Key_Enter or event.key()==Qt.Key_Return: 
             self.on_draw()
+        # Zoom out with minus
         elif event.key() == Qt.Key_Minus:
             self.glWidget.zoom(-1)
             self.glWidget.repaint()
+        # Zoom out with plus
         elif event.key() == Qt.Key_Plus:
             self.glWidget.zoom(1)
             self.glWidget.repaint()
-
-      
+    
+    #
+    # Create main_frame; initialize Objects and create Layout
+    #  
     def create_main_frame(self):
         self.main_frame = QWidget()
         self.main_frame.setMinimumHeight(280)
         
-        self.glWidget = GLWidget(self)
-        self.glWidget.real = re_min
-        self.glWidget.w = re_max-re_min
-        self.glWidget.imag = im_min
-        self.glWidget.h = im_max-im_min          
+        # OpenGL Widget
+        self.glWidget = GLWidget(self)         
         
         # Other GUI controls      
         self.textbox_re_min = QLineEdit()
         self.textbox_re_min_text = QLabel("ReMin: ")
         self.textbox_re_min.setMinimumWidth(55)
         
-        self.textbox_re_max = QLineEdit()
-        self.textbox_re_max_text = QLabel("ReMax: ")
-        self.textbox_re_max.setMinimumWidth(55)
-        
         self.textbox_im_min = QLineEdit()
         self.textbox_im_min_text = QLabel("ImMin: ")
         self.textbox_im_min.setMinimumWidth(55)
         
-        self.textbox_im_max = QLineEdit()
-        self.textbox_im_max_text = QLabel("ImMax: ")
-        self.textbox_im_max.setMinimumWidth(55)
+        self.textbox_delta = QLineEdit()
+        self.textbox_delta_text = QLabel("Delta: ")
+        self.textbox_delta.setMinimumWidth(55)
         
         self.textbox_max_iter = QLineEdit()
         self.textbox_max_iter_text = QLabel("Max Iterration: ")
         self.textbox_max_iter.setMinimumWidth(55)
-        
-        self.grid_cb = QCheckBox("Show Grid")
-        self.grid_cb.setChecked(False)
         
         self.cont_cb = QCheckBox("Continuous Coloring")
         self.cont_cb.setChecked(True)
@@ -147,9 +135,7 @@ class AppForm(QMainWindow):
         self.draw_button = QPushButton("Calculate && Draw")
         self.connect(self.draw_button, SIGNAL('clicked()'), self.on_draw)
         
-        #
         # Layout with box sizers
-        # 
         hbox = QHBoxLayout()
         grid = QGridLayout()
         hbox.addWidget(self.glWidget, 3)
@@ -158,35 +144,35 @@ class AppForm(QMainWindow):
  
         grid.addWidget(self.textbox_re_min , 0,1)
         grid.addWidget(self.textbox_re_min_text , 0,0)
-        grid.addWidget(self.textbox_re_max  , 1,1)
-        grid.addWidget(self.textbox_re_max_text  , 1,0)
-        grid.addWidget(self.textbox_im_min , 2,1)
-        grid.addWidget(self.textbox_im_min_text , 2,0)
-        grid.addWidget(self.textbox_im_max  , 3,1)
-        grid.addWidget(self.textbox_im_max_text , 3,0)
-        grid.addWidget(self.textbox_max_iter , 5,1)
-        grid.addWidget(self.textbox_max_iter_text , 5,0)   
-        grid.addWidget(self.grid_cb , 6,0,1,2)
-        grid.addWidget(self.cont_cb , 7,0,1,2)
-        grid.addWidget(self.norm_cb , 8,0,1,2)
-        
-        grid.addWidget(self.draw_button , 9,0,1,2)
-        grid.addWidget(QLabel(""), 10,0,2,2)
-        
+        grid.addWidget(self.textbox_im_min , 1,1)
+        grid.addWidget(self.textbox_im_min_text , 1,0)
+        grid.addWidget(self.textbox_delta , 2,1)
+        grid.addWidget(self.textbox_delta_text , 2,0)
+        grid.addWidget(self.textbox_max_iter , 3,1)
+        grid.addWidget(self.textbox_max_iter_text , 3,0)   
+        grid.addWidget(self.cont_cb , 4,0,1,2)
+        grid.addWidget(self.norm_cb , 5,0,1,2)
+        grid.addWidget(self.draw_button , 6,0,1,2)
+        grid.addWidget(QLabel(""), 7,0,2,2)
         
         self.main_frame.setLayout(hbox)
         self.setCentralWidget(self.main_frame)  
         
         # Set initial Foucus on glWidget
         self.glWidget.setFocus()
-        
+    
+    #
+    # Initialize statusbar
+    # Todo: Time to calculate, Coordinates under mouse pointer
+    #    
     def create_status_bar(self):
         self.status_text = QLabel("Ready")
-        self.coord_text = QLabel("Re(c): % 7f, Im(c) % 7f" % (0, 0))
-        
         self.statusBar().addWidget(self.status_text, 1)
-        self.statusBar().addWidget(self.coord_text, -1)
-        
+    
+    #
+    # Create main menu
+    # Todo: Save Screenshot
+    #   
     def create_menu(self): 
         # -- Menu Structure --
         # File
@@ -199,23 +185,28 @@ class AppForm(QMainWindow):
         
         quit_action = self.create_action("&Quit", slot=self.close,
             shortcut="Ctrl+Q", tip="Close the application")
-        
         self.add_actions(self.file_menu, (None, quit_action))
         
         self.help_menu = self.menuBar().addMenu("&Help")
+        
         about_action = self.create_action("&About",
             shortcut='F1', slot=self.on_about,
             tip='About the application')
-        
         self.add_actions(self.help_menu, (about_action,))
-
+    
+    #
+    # Add Action to menubar
+    #
     def add_actions(self, target, actions):
         for action in actions:
             if action is None:
                 target.addSeparator()
             else:
                 target.addAction(action)
-
+    
+    #
+    # Create new action
+    #
     def create_action(self, text, slot=None, shortcut=None,
                         icon=None, tip=None, checkable=False,
                         signal="triggered()"):
@@ -232,7 +223,10 @@ class AppForm(QMainWindow):
         if checkable:
             action.setCheckable(True)
         return action
-    
+
+#
+# Main Function
+#    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = AppForm()
